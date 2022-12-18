7032641e57aa90d04f6ca3c822d89efc694318d1
Pieter12345 <P.J.S.Kools@student.tudelft.nl>
2022-12-18 05:32:56 +0100

    Shade sfl4j-nop to prevent missing logger warning
    Fixes console warning about a missing slf4j logger implementation when calling `serial_connect()` for the first time in a new runtime environment.
    Note that this change causes a startup crash (class discovery bug) with MethodScript builds from before the day of this commit.

--- src/main/java/io/github/pieter12345/msserialconnection/MSSerialConnection.java
+++ src/main/java/io/github/pieter12345/msserialconnection/MSSerialConnection.java
@@ -39,6 +39,6 @@ public class MSSerialConnection extends AbstractExtension {
 	
 	@Override
 	public Version getVersion() {
-		return new SimpleVersion(1, 0, 0);
+		return new SimpleVersion(1, 0, 2);
 	}
 }
